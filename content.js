2fb4361b4664a67c986bbff68e88c4ac5cc4aef1
chris-jackson12 <cjackson12@protonmail.com>
2018-06-02 18:53:56 -0400

    files added

---
+++ content.js
@@ -0,0 +1,21 @@
+/**
+ * @author DarkShanks 
+ */
+
+// Obtain URL and check if it is a mobile link
+var url = window.location.href;
+var mobLinkTest = false;
+if(url.includes("//m.") || url.includes(".m.")){
+	mobLinkTest = true;
+}
+
+// If the link is a mobile link, replace it with a desktop link
+if(mobLinkTest){
+	if(url.includes("//m.")){
+		url = url.replace('m.', '');
+	}
+	else if(url.includes(".m.")){
+		url = url.replace('.m.', '.');
+	}
+	window.location.href = url;
+}
